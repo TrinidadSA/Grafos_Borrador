79ab605e558f7fb3689b7cd0cb4b89d2fddfc679
TrinidadSA <aguirretrini97@gmail.com>
2024-11-20 23:48:29 -0300

    agregue metodos en graphList que hay que crear

--- GraphList.java
+++ GraphList.java
@@ -127,33 +127,79 @@ public class GraphList<T extends Comparable<? super T>> implements Graph<T> {
     }
     
     //rastrear nodo
+       public List<T> rutaEntreVertices(T s, T n){
+        //comprobar que nodo s existe (sino, IllegalArgumentException)
+        //comprobar que nodo n existe (sino, IllegalArgumentException)
+        //Llamo a bfs(s,n); esto me devuelve la lista de numeros que son los de la ruta de s a n
+        //Funcion que elimina las peliculas de la lista, y reemplaza los numeros por Strings
+        //Deberia retornar la lista de tipo T (en nuestro caso van a ser Strings)
+        return null;
+    }
     
-    public int[] bfs(T s){
-        int ss = map.get(s);
-        int[] edgeTo = new int[V()];
-        boolean[] marked = new boolean[V()];
+    
+    
+    //bfs deberia recorrer el grafo desde el nodo s hasta encontrar el nodo n
+    //y devolver la ruta
+    public List<Integer> bfs(T s, T n){
+        int ss = map.get(s);  //obtengo indice del vertice s
+        int nn = map.get(n);  //obtengo indice del vertice s
+        int[] edgeTo = new int[V()];  //guarda desde donde viene el recorrido
+        boolean[] marked = new boolean[V()]; //gurda elementos marcados
         
-        Queue<Integer> q = new LinkedList<Integer>();
-        marked[ss] = true;
-        q.add(ss);
+        Queue<Integer> q = new LinkedList<Integer>();  //cola de los vertices que se van recorriendo
+        marked[ss] = true;  // marca verdadera la posición del índice desde donde parto
+        q.add(ss);  //agrego indice inicial a la cola
         
         
         while (!q.isEmpty()) {
             int v = q.poll();
-            for(T w : adj(keys[v])) {
-                int z = map.get(w);
-                if (!marked[z]){
-                    marked[z] = true;
-                    edgeTo[z] = v;
-                    q.add(z);
+            for(int w : adj[v]) {
+                if (!marked[w]){
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    q.add(w);
+                    
+                    if(w==nn){
+                        //esto retornaria la ruta en numeros
+                        return descubrirRuta(edgeTo, ss, nn); 
+                    }
                 }
             }
             
         }
-        return edgeTo;
+        return null;
     }
     
-    //arme arreglo entre act1 y act2 pasando arreglo de caminos    
-
     
+    private List<Integer> descubrirRuta(int[] path, int ini, int fin){
+        //Creo un linkedlist
+        //creo variable pos que me guarda la posicion que busco
+        //a pos le asigno fin
+        //agregar el numero fin a la lista
+        //deberia haber un ciclo, que mientras lo que tenga path en la posición pos 
+        //sea distinto a ini, 
+            //agrego a la lista lo que tiene path en la posición pos
+            //actualizo el pos, con lo que tiene path en la posicion pos
+        //cuando llego a posicion que teine guardado ini finalizo el ciclo
+        //agrego ini a la lista
+        //Todas las veces que agrego cosas deberian ser al principio de la lista
+        //creo que addFirst(elemento)  hace eso
+        
+        //retornar la lista creada
+        return null;
+    }
+    
+    
+    private List<T> obtenerRutaAcotada(List<Integer> path){
+        //En teoria el primer elemeto seria un actor, despues pelicula, despues actor
+        //crear una nueva lista del tipo T
+        //recorrer path
+            //deberia tratar un elemento si, otro no, otro si, otro no...otro si
+            //para cada indice de la lista deberia buscar su valor en keys[]
+            //cada elemento lo agrego a la nueva lista que cree
+        //retorno la lista creada -- en teoria deberia tener solo los nombres de los actores
+        return null;
+    }
+      
+
 }
